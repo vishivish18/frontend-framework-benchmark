c41d58872f3ca27e484e50173a95a2f8aa032406
Vishal Ranjan <vishivish18@gmail.com>
2017-05-22 01:12:44 +0530

    correcting angular 1.6.4 error, now promises need to be catched always

--- ng1/public/app/controllers/homeCtrl.js
+++ ng1/public/app/controllers/homeCtrl.js
@@ -5,18 +5,18 @@ angular.module('app')
                 // $scope.data = response.data.results
                 var start = performance.now();
                 console.log(start)
-                $scope.data= response.data//.results[0]
-                // $scope.data = []
-                // for (var i = 0; i < 10000; i++) {
-                //     $scope.data.push(result)
-                // }
-                // $timeout(function() {
-                //     // Logs when Angular is done processing repeater
-                //     console.log('Process time: ' + (new Date() - start));
-                //     console.log(performance.timing.domComplete)
-                //     var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-                //     console.log('Page load time is ' + loadTime);
-                // }); // Leave timeout empty to fire on next tick
+                $scope.data = response.data //.results[0]
+                    // $scope.data = []
+                    // for (var i = 0; i < 10000; i++) {
+                    //     $scope.data.push(result)
+                    // }
+                    // $timeout(function() {
+                    //     // Logs when Angular is done processing repeater
+                    //     console.log('Process time: ' + (new Date() - start));
+                    //     console.log(performance.timing.domComplete)
+                    //     var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+                    //     console.log('Page load time is ' + loadTime);
+                    // }); // Leave timeout empty to fire on next tick
                 $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
                     //you also get the actual event object
                     var end = performance.now();
@@ -28,6 +28,8 @@ angular.module('app')
                 });
 
             })
+            .catch(function(error) {
+                console.log(error)
+            });
 
     })
-
